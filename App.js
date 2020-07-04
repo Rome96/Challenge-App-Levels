b99bd772f3b00b1dbb8645a72ad4df9a13c8ef20
Turiano Romero <romero.guzman18@gmail.com>
2020-07-03 21:19:52 -0500

    agg icons

--- App.js
+++ App.js
@@ -1,73 +1,18 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+import {View, Text, StatusBar, StyleSheet, SafeAreaView} from 'react-native';
+import Header from './src/components/Header'
+const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-            </View>
-          </View>
+        <View>
+          <Header/>
+          <Text>Step One</Text>
+        </View>
       </SafeAreaView>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+// import {Ionicons} from '@expo/vector-icons';
+import {Text, View, Platform, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const HEIGHT = Platform.OS === 'ios' ? 60 : 56;
+
+const Header = () => {
+  return (
+    <View style={styles.container}>
+      <Icon name="md-arrow-back" color="#000" size={30} />
+      <Text style={styles.title}>Title</Text>
+      <Icon name="md-close" color="#000" size={30} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: HEIGHT,
+    width: '100%',
+    paddingVertical: 10,
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    backgroundColor: 'red',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '700',
+  },
+});
+
+export default Header;
